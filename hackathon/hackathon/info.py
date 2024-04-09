c3b026bbbc35402bb5ceda3a58e27b409539d3be
i_apex <jaim7viramgama@gmail.com>
2024-04-09 12:13:58 +0530

    initial phase

---
+++ hackathon/hackathon/info.py
@@ -0,0 +1,7 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'jimyjamy24@gmail.com'
+EMAIL_HOST_PASSWORD = 'nslv ftnv uofl jugo'
